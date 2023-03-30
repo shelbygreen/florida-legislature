3c903e1824cce161984840c66f45ad9178898914
shelby <53214352+shelbygreen@users.noreply.github.com>
2023-03-29 22:29:52 -0400

    got to clean florida house map file (#5)

---
+++ scripts/append.py
@@ -0,0 +1,181 @@
+"""script to add cc score field to the state legislative district geojson files
+
+process:
+1. pull data from MongoDB
+2. create tidy dataframe, that includes the cc score field
+3. loop through the files in reprojected-state-leg and add the cc score field
+4. store in dataframe
+5. export dataframe as geojson for senate or house
+6. QA/QC in Mapshaper 
+7. Export as 1 file: geojson-merge house/*.geojson > all-house.geojson or geojson-merge senate/*.geojson > all-senate.geojson
+8. QA/QC in Mapshaper (and reduce file size if needed)
+
+script 4/5
+"""
+
+# import libraries
+import re
+import pymongo
+import urllib
+import pandas as pd
+import geopandas as gpd
+from pathlib import Path
+
+
+# TEMP(matt): prevent IL from generating with sub-district divisions
+SKIP_STATES = ["IL"]
+
+
+def append_scores_for_chamber(legi_df, raw_dir, cleaned_dir):
+    for raw_shape in sorted(raw_dir.glob("*.geojson")):
+        # store the file in a gdf
+        raw_gdf = gpd.read_file(raw_shape)
+
+        # if there's a ccid field, append the score
+        if "ccid" not in raw_gdf.columns:
+            print("theres no ccid field. pass.")
+            continue
+
+        clean_gdf = raw_gdf.merge(legi_df[["ccid", "cc_score"]], on="ccid")
+
+        # if there are no scores for this state, continue
+        state_abbr = raw_shape.stem.split("-")[0]
+
+        if not len(clean_gdf) or state_abbr in SKIP_STATES:
+            print(
+                f"Skipping {raw_shape.name}, no cc scores to include (or a skip state)."
+            )
+            continue
+
+        # QA/QC check
+        print(f"Appended {raw_shape} is {len(clean_gdf)} vs. {len(raw_gdf)}")
+
+        clean_gdf.to_file(cleaned_dir / raw_shape.name, driver="GeoJSON")
+
+
+def extract_data_from_mongo():
+    """Returns the Regions and Representatives collections as Dataframes."""
+    print("connect to the production database")
+
+    username = urllib.parse.quote_plus("browse-data")
+    password = urllib.parse.quote_plus("climate-cabinet-1963")
+
+    client = pymongo.MongoClient(
+        f"mongodb+srv://{username}:{password}@cluster1.kmeus.mongodb.net/"
+    )
+
+    db = client["production-3-8-2022"]
+
+    regions = pd.DataFrame(list(db.region.find()))
+    reps = pd.DataFrame(list(db.representative.find()))
+
+    client.close()
+
+    return (regions, reps)
+
+
+if __name__ == "__main__":
+    regions, reps = extract_data_from_mongo()
+
+    """ create tidy dataframe """
+    print("creating tidy dataframe")
+
+    # expanded incumbents
+    df_incumbents = pd.concat(
+        [
+            regions.drop(["incumbents"], axis=1),
+            regions["incumbents"].apply(pd.Series)[0].apply(pd.Series)[["name", "rep"]],
+            regions["incumbents"].apply(pd.Series)[1].apply(pd.Series)[["name", "rep"]],
+        ],
+        axis=1,
+    )
+
+    # filtered dataframe to just show necessary columns for state house & senate
+    regions = df_incumbents[
+        (df_incumbents["_cls"] == "Region.District.StateLegDistLower")
+        | (df_incumbents["_cls"] == "Region.District.StateLegDistUpper")
+    ][["state_abbr", "geoid", "ccid", "district_type", "name", "rep"]]
+
+    # rename columns
+    regions.columns = [
+        "state_abbr",
+        "geoid",
+        "ccid",
+        "chamber",
+        "district_name",
+        "incumbent_name_1",
+        "incumbent_name_2",
+        "district_1",
+        "district_2",
+    ]
+
+    # grab only necessary columns for future merging
+    regions = regions[["state_abbr", "geoid", "ccid", "district_name"]]
+
+    # filtered representative dataframe
+    reps = pd.concat(
+        [reps.drop(["office"], axis=1), reps["office"].apply(pd.Series)], axis=1
+    )[
+        [
+            "full_name",
+            "cc_score",
+            "district_ccid",
+            "district",
+            "seat_number",
+            "party",
+            "is_current",
+        ]
+    ]
+
+    # get average score (for current legislators)
+    representative = (
+        reps.query("is_current == True")
+        .groupby("district_ccid")
+        .mean()
+        .reset_index(drop=False)[["district_ccid", "cc_score"]]
+    )
+
+    # fill NaN with 999 for cc score
+    representative["cc_score"] = representative["cc_score"].fillna("999")
+
+    # round score to integer
+    representative["cc_score"] = representative["cc_score"].astype(int)
+
+    # merge the two dataframes
+    ccscorecard_legislator = regions.merge(
+        representative, left_on="ccid", right_on="district_ccid", how="inner"
+    ).drop(columns=["district_ccid"])
+
+    # manual edits -- append districts with vacant seats
+
+    # # set PA House District 113 cc score as SEAT VACANT (444)
+    # # special election is nov 2021
+    # ccscorecard_legislator.loc[len(ccscorecard_legislator.index)] = [
+    #     "PA",
+    #     "42113",
+    #     "42113L",
+    #     "State House District 113",
+    #     444,
+    # ]
+
+    # tidy dataframe
+    ccscorecard_legislator = ccscorecard_legislator.sort_values(
+        by=["geoid", "cc_score"], ascending=True
+    )
+
+    """ loop through files and add the cc score """
+    print("Adding CC Scores to shapes")
+    raw_dir = Path("data/geospatial/clean")
+    cleaned_dir = Path("data/geospatial/append")
+
+    # if the output directories don't exist, create them
+    for output in [cleaned_dir / "house", cleaned_dir / "senate"]:
+        output.mkdir(parents=True, exist_ok=True)
+
+    # handle for house
+    append_scores_for_chamber(
+        ccscorecard_legislator, raw_dir / "house", cleaned_dir / "house"
+    )
+    append_scores_for_chamber(
+        ccscorecard_legislator, raw_dir / "senate", cleaned_dir / "senate"
+    )
---
+++ scripts/process.py
@@ -0,0 +1,163 @@
+""" script to convert shp to geojson
+credit to @openstates/openstates-geo
+
+data source: US Census Bureau Tiger (www2.census.gov/geo/tiger/TIGER2020)
+data documentation: https://www2.census.gov/geo/pdfs/maps-data/data/tiger/tgrshp2019/TGRSHP2019_TechDoc.pdf
+data fields:
+- - STATEFP, state fips code, 12
+- - SLDLST, state leg district number, 010
+- - GEOID, state leg chamber + district number, State House District 10
+- - LSAD, chamber level, LL
+- - LSY, legislative session year, 2018
+- - MTFCC, MAF/TIGER feature class code, G5220
+- - FUNCSTAT, Current functional status, N
+- - ALAND, district land area, 6837821870 (units?)
+- - AWATER, district water area, 42620795 (unts?)
+- - INTPTLAT, latitude, +30.2926978
+- - INTPTLON, longitude, -082.7108549
+
+process:
+step 1: load shp into geodataframe
+step 2: add column for bounds
+step 3: parse into json
+step 4: add fields for chamber, district, state, ...
+step 5: export as geojson
+
+script 2/5
+
+TODO: MD, NJ, NY, and WI house won't output for this script. figure out why. 
+"""
+
+# import libraries
+import os
+import sys
+import json
+import glob
+import subprocess
+import us
+import geopandas as gpd
+from pathlib import Path
+
+IN = Path("data/geospatial/fetch")
+OUT = Path("data/geospatial/process")
+
+# import openstates_metadata as metadata
+
+
+def shp_to_geojson(geojson_path, is_state_file):
+    # read in the shp and store it in a geodataframe
+    gdf = gpd.read_file(geojson_path)
+
+    # add bounds
+    # gdf['bounds'] = gdf.bounds.round(2).apply(lambda row: list(row), axis=1)
+
+    chamber = "House" if geojson_path.stem.endswith("sldl") else "Senate"
+
+    # parse to geojson
+    gdf_parsed = gdf.to_json()
+    geojson = json.loads(gdf_parsed)
+
+    # add fields to each feature -- geoid, bounds, state abbr, chamber, district
+    for feature in geojson["features"]:
+        # rename keys in state files
+        if is_state_file:
+            for key in [k for k in feature["properties"].keys()]:
+                new_key = key.replace("10", "")
+                feature["properties"][new_key] = feature["properties"].pop(key)
+
+        state = us.states.lookup(feature["properties"]["STATEFP"])
+        geoid = feature["properties"]["GEOID"]
+        # bounds = feature["properties"]["bounds"]
+
+        if is_state_file:
+            name = state.name
+            ccid = state.fips
+        else:
+            name = (
+                us.states.lookup(feature["properties"]["STATEFP"]).name
+                + " "
+                + feature["properties"]["NAMELSAD"]
+            )
+
+            # create new features
+            ccid = (
+                feature["properties"]["GEOID"] + "U"
+                if feature["properties"]["LSAD"] == "LU"
+                else feature["properties"]["GEOID"] + "L"
+            )
+
+            district = (
+                feature["properties"]["SLDLST"].lstrip("0")
+                if "SLDLST" in feature["properties"].keys()
+                else feature["properties"]["SLDUST"].lstrip("0")
+            )
+
+        feature["properties"] = {
+            "state_fips": state.fips,
+            "state_abbr": state.abbr,
+            "geoid": geoid,
+            "ccid": ccid,
+            "name": name,
+            # "bounds": bounds,
+        }
+
+        if not is_state_file:
+            feature["properties"]["chamber"] = chamber
+            feature["properties"]["district"] = district
+        else:
+            # if the shape is a state, and the shape is included in the final
+            # shapefile, then we have no data on that state, and we want the
+            # entire state to have a flag value for cc_score
+            feature["properties"]["cc_score"] = 999
+
+    # export to geojson file
+    region_type = "state" if is_state_file else chamber.lower()
+    output_path = OUT / f"{state.abbr}-{region_type}.geojson"
+    print(f" {state.abbr}-{region_type} shp => {output_path.name}")
+
+    # if the output directory doesn't exist, create it
+    if not output_path.parent.exists():
+        output_path.parent.mkdir(parents=True)
+
+    with open(output_path, "w+") as geojson_file:
+        json.dump(geojson, geojson_file)
+
+
+if __name__ == "__main__":
+    # check to see if all of the files were downloaded
+    if len(sys.argv) == 1:
+        files = sorted(IN.glob("tl*.shp"))
+        if len(files) != 149:
+            raise AssertionError(f"Expecting 149 shapefiles, got {len(files)}).")
+    else:
+        files = sys.argv[1:]
+
+    # convert from shp to geojson
+    for file in files:
+        is_state_file = "state10" in file.name
+
+        # create geojson in source folder
+        newfilename = file.with_suffix(".geojson")
+        if os.path.exists(newfilename):
+            print(newfilename, "already exists, skipping")
+        else:
+            print(file, "=>", newfilename)
+            subprocess.run(
+                [
+                    "ogr2ogr",
+                    "-where",
+                    f"GEOID{'10' if is_state_file else ''} NOT LIKE '%ZZZ'",
+                    "-t_srs",
+                    "crs:84",
+                    "-f",
+                    "GeoJSON",
+                    newfilename,
+                    file,
+                ],
+                check=True,
+            )
+        # create geojson in all folder
+        shp_to_geojson(newfilename, is_state_file)
+
+    # finished message
+    print("Done converting shp to geojson")
